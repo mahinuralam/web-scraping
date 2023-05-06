0760ded0ed2004d143e923be927b864e3053c95a
Md. Mahinur Alam <64003114+mahinuralam@users.noreply.github.com>
2023-05-06 20:09:27 +0600

    modifications on routes/user.py

--- routes/user.py
+++ routes/user.py
@@ -7,9 +7,9 @@ from config.db import session
 from models.index import Products
 from schema.index import Products
 
-user_router = APIRouter()
+product_router = APIRouter()
 
-@user_router.get("/query_by_user_id/{user_id}")
+@product_router.get("/query_by_user_id/{user_id}")
 async def read_user(user_id: int):
     try:
         return session.query(Products).filter(Products.id == user_id).first()
@@ -18,7 +18,7 @@ async def read_user(user_id: int):
         raise HTTPException(status_code=404, detail="User not found")
     
 
-@user_router.get("/get_all_user")
+@product_router.get("/get_all_user")
 async def read_user():    
     try:
         res = session.query(Products).all()
@@ -27,18 +27,18 @@ async def read_user():
     return res
 
 
-@user_router.post("/regiser_user")
-async def write_user(user : Products):
+@product_router.post("/get_products")
+async def write_product(product : Products):
+    search_product = ['HP', 'Asus', 'Dell', 'Razor', 'Aser']
+    
     try:
-        user_data = Products(
-                name = user.name,
-                price = user.price,
-                ulr = user.url
+        product_data = Products(
+                name = product.name,
         )
-        session.add(user_data)
+        session.add(product_data)
         session.commit()
-        session.refresh(user_data)
-        return session.query(Products).filter_by(id=user_data.id).first()
+        session.refresh(product_data)
+        return session.query(Products).filter_by(id=product_data.id).first()
     except Exception as e:
         print(e) 
         session.rollback()
